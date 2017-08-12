396ca753fe96eeb0bafd89ab7ca7bd7fc6a4331a
user <ekanshsharmacse11@gmail.com>
2017-08-12 12:05:36 +0530

    Add webpack.

---
+++ script-1.js
@@ -0,0 +1,5 @@
+var message = require("./script-2");
+alert(message);
+
+
+//node_modules/.bin/webpack script-1.js ./bundle.js
---
+++ script-2.js
@@ -0,0 +1,3 @@
+var message = "Ekansh Sharma";
+
+module.exports = message;
